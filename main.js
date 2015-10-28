4c74ae5137a625bf2d00c0f034a91cdf5af1830e
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-28 16:21:55 -0700

    added beer data

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ $(document).ready(function(){
 		
 		var test=beerData.Beer.map(function(obj){
 			return {
-				price:obj.price
+				"id": obj.id,
+				"item": obj.item,
+				"price":obj.price,
+				"style": obj.style,
+				"abv": obj.abv,
+				"description": obj.description,
+				"allergies": obj.allergies,
+				"favorite": obj.favorite,
+				"bottle": obj.bottle,
+				"draught": obj.draught
 			};
 		});
 
